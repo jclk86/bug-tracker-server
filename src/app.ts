5009734b64ef2d29aa9ffe3eb35b060dc8ca9530
John Chan <johnchancontact@gmail.com>
2021-04-19 10:35:42 -0400

    test commit

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ app.get('/test', (req, res) => {
   res.send('Hello World');
 });
 
+console.log('hi');
+
 app.get('/', (req, res) => {
   res.status(404).send({ error: { message: 'Page Not Found' } });
 });
