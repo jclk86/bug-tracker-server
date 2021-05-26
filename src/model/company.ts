28b06917a562c32ff4db9beca6dd3317f261a6f7
John Chan <johnchancontact@gmail.com>
2021-05-26 15:33:26 -0400

    FEAT: removed comments from company model

--- src/model/company.ts
+++ src/model/company.ts
@@ -2,23 +2,19 @@
 import db from '../database/config';
 import { ICompany } from '../schema/company';
 
-// getAllCompanies
 async function getAllCompanies(): Promise<ICompany[]> {
   return await db<ICompany>('company').returning('*');
 }
 
-// getCompanyByName
 async function getCompanyByName(name: string): Promise<ICompany[]> {
   return await db<ICompany>('company').returning('*').where({ name });
 }
 
-// create company
 async function createCompany(newCompany: ICompany): Promise<ICompany> {
   await db<ICompany>('company').insert(newCompany);
   return newCompany;
 }
 
-// updateCompany
 // async function updateCompany(editedCompany): Promise<ICompany> {
 //   await db<ICompany>('company')
 //     .where('name', editedCompany.name)
