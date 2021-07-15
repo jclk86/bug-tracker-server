42c7187fa3f20c2ecc86a00acf9c55479b9b7750
John Chan <johnchancontact@gmail.com>
2021-07-15 16:17:07 -0400

    FEAT: implemented refresh token blacklisting

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -35,9 +35,11 @@ export const signin = async (req: Request, res: Response): Promise<void> => {
   };
 
   const accessToken = await jwt.sign(payload, process.env.ACCESS_JWT_KEY, { expiresIn: '60s' });
-  const refreshToken = await jwt.sign(payload, process.env.REFRESH_JWT_KEY, { expiresIn: '7d' });
+  const refreshToken = await jwt.sign(payload, process.env.REFRESH_JWT_KEY, { expiresIn: '1y' });
 
   if (!accessToken || !refreshToken) throw new CustomError(401, 'Invalid token');
+
+  // place into header on client side
   //! samesite requires cors header to be set
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
@@ -49,27 +51,49 @@ export const signin = async (req: Request, res: Response): Promise<void> => {
 };
 // ! https://marmelab.com/blog/2020/07/02/manage-your-jwt-react-admin-authentication-in-memory.html
 // ! then do redis
-export const signRefreshToken = async (req: Request, res: Response): Promise<void> => {
+// !execute on front end when client makes request but access token is expired
+export const postRefreshToken = async (req: Request, res: Response): Promise<void> => {
   const { refreshToken } = req.cookies;
+  const user = req['user'];
+
+  // or we can check the refresh token first and if not found, then use cookie
 
   // ! just return nothing - give as little as possible
   if (!refreshToken) throw new CustomError(401, 'User is not authenticated');
-  // store token
+
+  const blacklistedTokens = await client.lrange(user.id, 0, 999999);
+
+  // check if token exists among blacklisted tokens
+  if (blacklistedTokens.indexOf(refreshToken) > -1) throw new CustomError(401, 'Unauthorized');
+
+  // https://dev.to/chukwutosin_/how-to-invalidate-a-jwt-using-a-blacklist-28dl
+  //  https://dev.to/mr_cea/using-redis-for-token-blacklisting-in-node-js-42g7
 
   // const { refresh_token, blacklisted } = await getRefreshToken(refreshToken);
-  jwt.verify(refreshToken, process.env.REFRESH_JWT_KEY, (err, user) => {
+  await jwt.verify(refreshToken, process.env.REFRESH_JWT_KEY, async (err, user) => {
     if (err) throw new CustomError(403, 'Invalid token');
+
     // destructures email from user to override expiresIn from user
-    const accessToken = jwt.sign({ email: user.email }, process.env.ACCESS_JWT_KEY, {
+    const accessToken = jwt.sign({ id: user.id }, process.env.ACCESS_JWT_KEY, {
       expiresIn: '20s'
     });
-    // place into header on client side
+
     res.json({ accessToken: accessToken });
   });
 };
 
 export const signOut = async (req: Request, res: Response): Promise<void> => {
   // clear the access token in client end by setting the in-memory variable for accessToken = null
+  const { refreshToken } = req.cookies;
+
+  await jwt.verify(refreshToken, process.env.REFRESH_JWT_KEY, async (err, user) => {
+    if (err) throw new CustomError(403, 'Invalid token');
+
+    const userId = user.id;
+    // blacklist
+    await client.lpush(userId, refreshToken);
+  });
+
   res.clearCookie('refreshToken');
   res.redirect('/login');
 };
--- src/route/auth.ts
+++ src/route/auth.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import { catchAsync } from './utilities';
-import { signin, signRefreshToken, signOut } from '../controller/auth';
+import { signin, postRefreshToken, signOut } from '../controller/auth';
 import { requireAuth } from '../middleware/jwt-auth';
 
 const authRouter = Router();
 
 authRouter.post('/login', catchAsync(signin));
 
-authRouter.patch('/logout', catchAsync(signOut));
+authRouter.delete('/logout', catchAsync(signOut));
 
 authRouter.use(catchAsync(requireAuth));
 
-authRouter.post('/refresh', catchAsync(signRefreshToken));
+authRouter.post('/refresh', catchAsync(postRefreshToken));
 
 export default authRouter;
