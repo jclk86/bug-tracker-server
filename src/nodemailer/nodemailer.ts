91d65bf553f33822c0a7831acbca7314b294278f
John Chan <johnchancontact@gmail.com>
2021-07-28 17:20:14 -0400

    FIX: removed previous nodemailer transport instance configurations

--- src/nodemailer/nodemailer.ts
+++ src/nodemailer/nodemailer.ts
@@ -1,15 +1,5 @@
 import nodemailer from 'nodemailer';
 
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.gmail.com',
-//   port: 465,
-//   secure: true, // true for 465, false for other ports
-//   auth: {
-//     user: 'jcnyg1986@gmail.com', // generated ethereal user
-//     pass: 'IsTh1sReallyWeird?1986' // generated ethereal password
-//   }
-// });
-
 export const transporter = nodemailer.createTransport({
   host: 'smtp-mail.outlook.com',
   secureConnection: false,
